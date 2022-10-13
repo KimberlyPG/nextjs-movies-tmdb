45af47449da075a031203b6b8d134f8209578b1b
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-10-12 19:18:17 -0600

    style details in the posters

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
       }
     }    
     `);
-      // console.log("queryMovies", queryMovies.allTmdbMovieTopRated.nodes)
+      console.log("queryMovies", queryMovies.allTmdbMovieTopRated.nodes)
     return (
       <div className="mx-12 mt-10">
         <div className="flex space-x-5 mb-10">
@@ -31,11 +31,17 @@ const Home = () => {
         </div>
         <h2 className="text-lg font-bold">Top rated movies</h2>
         <div className="grid grid-cols-8 gap-4">
-             {queryMovies?.allTmdbMovieTopRated?.nodes.slice(0, 16).map((poster) => (
-                <img 
-                  className="w-48 rounded-xl"
-                  src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${poster.poster_path.original}`} 
-                />
+             {queryMovies?.allTmdbMovieTopRated?.nodes.slice(0, 16).map((item) => (
+              <div>
+                <div className="relative hover:opacity-70 h-full">
+                  <p className="text-xs text-green-600 absolute m-3 bg-gray-200 bg-opacity-300 p-2 rounded-full">{item.vote_average}</p>
+                  <img 
+                    className="h-full rounded-xl"
+                    src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${item.poster_path.original}`} 
+                  />
+                </div>
+                  {/* <p className="text-sm truncate">{item.title}</p> */}
+              </div>
               ))}
         </div>
       </div>
