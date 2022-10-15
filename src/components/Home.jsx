0951aa78d5b94d7b46e49f985ddab98a1f15db1e
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-10-15 17:41:56 -0600

    toggle button to change between movies data and tv shows data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
 import * as React from "react"
+import { useState } from "react";
 import {  graphql,useStaticQuery } from "gatsby";
 import MovieCard from "./Movie-card";
 import Shows from "./Shows";
 
 const Home = () => {
-    
+    const [moviesView, setMoviesView] = useState(true);
+
+    const toggle = () => setMoviesView(prewState => !prewState);
+    console.log("moviesView", moviesView)
+
     const queryMovies =  useStaticQuery(graphql`
     query MyQuery {
       allTmdbMovieTopRated(sort: {fields: release_date, order: DESC}) {
@@ -25,21 +30,32 @@ const Home = () => {
     }    
     `);
       console.log("queryMovies", queryMovies.allTmdbMovieTopRated.nodes)
+  
     return (
       <div className="mx-12 mt-10">
         <div className="flex space-x-5 mb-10">
-          <button className="text-gray-500">Movies</button>
-          <button className="text-gray-500">Tv Shows</button>
+          <button className="text-gray-500" onClick={toggle}>Movies</button>
+          <button className="text-gray-500" onClick={toggle}>Tv Shows</button>
         </div>
-        <h2 className="text-lg font-bold">Top rated movies</h2>
-        <div className="grid grid-cols-8 gap-4">
-             {queryMovies?.allTmdbMovieTopRated?.nodes.slice(0, 16).map((item) => (
-              <div>
-                <MovieCard data={item}/>           
-                <Shows />     
-              </div>
-              ))}
+        {moviesView === true ? 
+        (
+        <div>
+          <h2 className="text-lg font-bold">Top rated movies</h2>
+          <div className="grid grid-cols-8 gap-4">
+              {queryMovies?.allTmdbMovieTopRated?.nodes.slice(0, 16).map((item) => (
+                <div>
+                  <MovieCard data={item}/>           
+                  <Shows />     
+                </div>
+                ))}
+          </div>
         </div>
+        ):(
+          <div>
+            <h1>Tv shows</h1>
+          </div>
+        )
+        }
       </div>
     )
 }
--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -25,7 +25,7 @@ const Shows = () => {
     
     return (
         <div>
-            shows component
+            {/* shows component */}
         </div>
     )
 }
