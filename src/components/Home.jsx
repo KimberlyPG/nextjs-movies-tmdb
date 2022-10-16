8b8a0714679fea459bfdb4f90899be9ca95376a4
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-10-16 17:11:41 -0600

    new styles for movies and tv shows buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,10 +11,26 @@ import { responsive } from "../utils/carousel_responsive";
 
 const Home = () => {
     const [moviesView, setMoviesView] = useState(true);
+    const [moviesButtonBg, setMoviesButtonBg] = useState('#1B5BA9');
+    const [showsButtonBg, setShowsButtonBg] = useState('transparent');
+    const [moviesButtonColor, setMoviesButtonColor] = useState('#FFFFFF');
+    const [showsButtonColor, setShowsButtonColor] = useState('#C2C8CD');
 
-    const toggle = (value) => setMoviesView(value);
-    console.log("moviesView", moviesView)
-
+    const toggle = (value) => {
+      setMoviesView(value);
+      if(value === true) {
+        setMoviesButtonBg('#1B5BA9');
+        setMoviesButtonColor('#FFFFFF');
+        setShowsButtonBg('transparent');
+        setShowsButtonColor('#C2C8CD');
+      }
+      else {
+        setMoviesButtonBg('transparent');
+        setMoviesButtonColor('#C2C8CD');
+        setShowsButtonBg('#1B5BA9');
+        setShowsButtonColor('#FFFFFF')
+      }
+    }
     const queryMoviesAndTv =  useStaticQuery(graphql`
     query MyQuery {
       movies: allTmdbMovieTopRated(sort: {fields: release_date, order: DESC}) {
@@ -52,14 +68,14 @@ const Home = () => {
     return (
       <div className="mx-12 mt-10">
         <div className="flex space-x-5 mb-10">
-          <button className="text-gray-500" onClick={() => toggle(true)}>Movies</button>
-          <button className="text-gray-500" onClick={() => toggle(false)}>Tv Shows</button>
+          <button className="text-gray-500 rounded-xl px-4" onClick={() => toggle(true)} style={{backgroundColor: `${moviesButtonBg}`, color: `${moviesButtonColor}`}}>Movies</button>
+          <button className="text-gray-500 rounded-xl px-4" onClick={() => toggle(false)} style={{backgroundColor: `${showsButtonBg}`, color: `${showsButtonColor}`}}>Tv Shows</button>
         </div>
         {moviesView === true ? 
         (
         <>
           <h2 className="text-lg font-bold px-3">Top rated movies</h2>
-          <Carousel responsive={responsive} >
+          <Carousel responsive={responsive} centerMode={true} >
               {queryMoviesAndTv?.movies.nodes.map((item) => (
                 <div className="h-full p-3">
                   <MovieCard data={item} />
@@ -70,7 +86,7 @@ const Home = () => {
         ):(
         <>
           <h2 className="text-lg font-bold px-3">Top rated tv shows</h2>
-          <Carousel responsive={responsive} >
+          <Carousel responsive={responsive} centerMode={true} >
               {queryMoviesAndTv?.tv.nodes.map((item) => (
                 <div className="h-full p-3">
                   <TVshowCard data={item} />
