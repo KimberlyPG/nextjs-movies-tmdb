4a43b8384dc91fecf7c09f96bae24c2cd41e0f84
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-10-20 18:20:43 -0600

    movie information and style

--- src/components/Search-list.jsx
+++ src/components/Search-list.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const SearchList = ({ item }) => {
   return (
-    <div>
-        <p>{item.title}</p>
-        <img src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${item.poster_path}`} alt="" />
+    <div className='flex flex-row border p-5'>
+        <img 
+            className="w-36"
+            src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${item.poster_path}`} 
+            alt={`${item.title} poster`}
+            />
+            <span>
+                <h3>{item.title}</h3>
+                <p>{item.overview}</p>
+            </span>
     </div>
   )
 }
