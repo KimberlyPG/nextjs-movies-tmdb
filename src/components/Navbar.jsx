3235141064a4ae160aefcb9d957a1b3f77d6162e
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-10-27 20:04:30 -0600

    logo and searchbar added to topbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,6 @@ const Navbar = ({ setMoviesView }) => {
       }
   return (
     <div>   
-        <SearchBar />
         <div className="flex space-x-5 mb-10">
           <button className="text-gray-500 rounded-xl px-4" onClick={() => toggle(true)} style={{backgroundColor: `${moviesButtonBg}`, color: `${moviesButtonColor}`}}>Movies</button>
           <button className="text-gray-500 rounded-xl px-4" onClick={() => toggle(false)} style={{backgroundColor: `${showsButtonBg}`, color: `${showsButtonColor}`}}>Tv Shows</button>
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import SearchBar from './SearchBar'
+
+import logo from '../assets/logo.svg'
+
 const Topbar = () => {
 return (
-    <div className='flex bg-[#1B5BA9] h-12 items-center'>
-        <div className="flex">
-            <p className="text-white font-bold justify-end">Home</p>
+    <div className='flex bg-[#FFFFFF] h-14 items-center px-8'>
+        <img
+            className='w-36' 
+            src={logo} 
+            alt="page logo" 
+        />
+        <div className='grid justify-center w-screen mt-1'>
+            <SearchBar />
         </div>
     </div>
   )
