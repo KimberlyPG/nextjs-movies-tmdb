71a328067caae31252a123ab12f0cdcdbf335c17
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-11-01 04:34:12 -0600

    new movie page

--- src/components/MovieAndTv-card.jsx
+++ src/components/MovieAndTv-card.jsx
@@ -1,9 +1,10 @@
 import * as React from "react"
+import { navigate } from "gatsby";
 
 const MovieAndTvCard = ({ data }) => {
 
     return (
-        <div className="relative hover:opacity-70 h-full cursor-pointer">
+        <div className="relative hover:opacity-70 h-full cursor-pointer" onClick={() => navigate(`/movie/${data.name}`)}>
             {data.vote_average && 
                 <p className="text-xs text-green-600 absolute m-3 bg-gray-200 bg-opacity-300 p-2 rounded-full">{data.vote_average}</p>
             }
@@ -12,13 +13,13 @@ const MovieAndTvCard = ({ data }) => {
                     className="h-full rounded-xl"
                     src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${data.poster_path.original}`} 
                 />
-            ):
-            ( 
+                ):
+                ( 
                 <img 
                     className="h-full rounded-xl"
                     src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${data.poster_path}`} 
                 />
-            )
+                )
             }
         </div>         
     )
---
+++ src/pages/movie/[...].jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Details = ({ params }) => {
+    const param = params[`*`]
+
+    return (
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default Details;
