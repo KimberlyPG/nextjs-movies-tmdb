76366b97c6274cbdefb0af2ac88ef64641d0f52f
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-11-05 18:20:37 -0600

    styles for the page

--- src/pages/details/[...].jsx
+++ src/pages/details/[...].jsx
@@ -25,7 +25,7 @@ const Details = ({ params, location }) => {
             <div className="w-full" 
                 style={{
                     backgroundSize: 'cover', 
-                    backgroundImage:`linear-gradient(0deg, rgba(2,173,231,0.5), rgba(2,173,231,0.5)), url(https://image.tmdb.org/t/p/w1280/${data?.backdrop_path})`,
+                    backgroundImage:`linear-gradient(0deg, rgba(1, 124, 128,0.8), rgba(1, 124, 128,0.8)), url(https://image.tmdb.org/t/p/w1280/${data?.backdrop_path})`,
                 }}>
                 <div className="flex p-10 mx-20 justify-center" >
                     <img 
@@ -33,9 +33,9 @@ const Details = ({ params, location }) => {
                         src={`https://image.tmdb.org/t/p/w1280/${data?.poster_path}`} 
                     />
                     <span className="flex flex-col w-2/5 ml-20">
-                        <h2 className="text-2xl text-gray-500">{data?.title}</h2>
-                        <h3>Overview</h3>
-                        <p>{data?.overview}</p>
+                        <h2 className="text-2xl text-white font-bold text-3xl">{data?.title}</h2>
+                        <h3 className="text-white font-bold text-lg">Overview</h3>
+                        <p className="text-gray-100 text-lg">{data?.overview}</p>
                     </span>
                 </div>
             </div>
