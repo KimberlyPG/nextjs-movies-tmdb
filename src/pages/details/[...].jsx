ba303e4aa4b018ac3ef048cae57b5d45919bb62e
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-11-05 20:53:59 -0600

    vote average agreggated

--- src/pages/details/[...].jsx
+++ src/pages/details/[...].jsx
@@ -23,39 +23,48 @@ const Details = ({ params, location }) => {
     
     return (
         <Layout>
-            <div className="w-full" 
-                style={{
-                    backgroundSize: 'cover', 
-                    backgroundImage:`linear-gradient(0deg, rgba(1, 124, 128,0.8), rgba(1, 124, 128,0.8)), url(https://image.tmdb.org/t/p/w1280/${data?.backdrop_path})`,
-                }}>
-                <div className="flex p-10 mx-20 justify-center" >
-                    <div>
-                        <img 
-                            className="rounded-xl w-96"
-                            src={`https://image.tmdb.org/t/p/w1280/${data?.poster_path}`} 
-                        />
-                        <span className="flex text-white space-x-5 font-semibold">
-                            <p>{data?.release_date.split('-')[0]}</p>
-                            <p>{minutesToHours(data?.runtime)}</p>
-                        </span>
-                    </div>
-                    <div className="flex w-3/5">
-                        <div className="mx-20">
-                            <h2 className="text-2xl text-white font-bold text-3xl">{data?.title}</h2>
-                            <div className="flex text-white space-x-7">
-                                <span className="flex flex-col">
-                                    <h3 className="text-white font-bold text-lg">Overview</h3>
-                                    <p className="text-gray-100 text-lg font-semibold">{data?.overview}</p>
-                                </span>
-                                <div className="flex flex-col">
-                                    <p className="font-bold">Genres</p>
-                                    {data?.genres.map((genre) => (
-                                        <p className="border border-green-300 rounded-sm mb-2 text-center px-2">{genre.name}</p>
-                                        ))
-                                    }
+            <div>
+                <div className="w-full" 
+                    style={{
+                        backgroundSize: 'cover', 
+                        backgroundImage:`linear-gradient(0deg, rgba(1, 124, 128,0.8), rgba(1, 124, 128,0.8)), url(https://image.tmdb.org/t/p/w1280/${data?.backdrop_path})`,
+                        backgroundAttachment: 'fixed'
+                    }}>
+                    <div className="flex p-10 mx-20 justify-center" >
+                        <div>
+                            <img 
+                                className="rounded-xl w-96"
+                                src={`https://image.tmdb.org/t/p/w1280/${data?.poster_path}`} 
+                            />
+                            <span className="flex text-white space-x-5 font-semibold text-lg ml-3">
+                                <p>{data?.release_date.split('-')[0]}</p>
+                                <p>{minutesToHours(data?.runtime)}</p>
+                            </span>
+                        </div>
+                        <div className="flex w-3/5">
+                            <div className="mx-20 space-y-5">
+                                <div>
+                                    <h2 className="text-2xl text-white font-bold text-3xl">{data?.title}</h2>
+                                    <p className="text-gray-100">{data?.tagline}</p>
                                 </div>
-                            </div>
-                        </div>          
+                                <div className="flex text-white space-x-7">
+                                    <span className="flex flex-col">
+                                        <h3 className="text-white font-bold text-lg">Overview</h3>
+                                        <p className="text-gray-100 text-lg font-semibold">{data?.overview}</p>
+                                        <div>
+                                            <p>{data?.vote_average}</p>
+                                        </div>
+                                    </span>
+                                    <div className="flex flex-col">
+                                        <p className="font-bold">Genres</p>
+                                        {data?.genres.map((genre) => (
+                                            <p className="border border-green-300 rounded-sm mb-2 text-center px-2">{genre.name}</p>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            </div>          
+                        </div>
                     </div>
                 </div>
             </div>
