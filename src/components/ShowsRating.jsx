bc9855d1fa5e02bd7dbb671fc8e4a73159671222
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-11-22 03:31:47 -0600

    responsive design for details page

--- src/components/ShowsRating.jsx
+++ src/components/ShowsRating.jsx
@@ -4,8 +4,8 @@ import Rating from "@mui/material/Rating";
 const ShowsRating = ({ data}) => {
   return (
       <div className="flex space-x-2 mt-5 text-white">
-          <Rating value={data?.vote_average/2} readOnly={true} precision={0.5}/>
-          <p className="text-bold">{data?.vote_average}/10</p>
+          <Rating className='xs:text-lg sm:text-2xl' value={data?.vote_average/2} readOnly={true} precision={0.5}/>
+          <p className="text-bold xs:text-sm lg:text-base">{data?.vote_average}/10</p>
       </div>
   )
 }
--- src/pages/details/[name].jsx
+++ src/pages/details/[name].jsx
@@ -99,38 +99,38 @@ const Details = ({ location }) => {
                     backgroundImage:`linear-gradient(0deg, rgba(1, 124, 128,0.8), rgba(1, 124, 128,0.8)), url(https://image.tmdb.org/t/p/w1280/${data?.backdrop_path})`,
                     backgroundAttachment: 'fixed',
                 }}>
-                <div className="flex xs:flex-col lg:flex-row lg:p-10 xs:p-4 lg:mx-20 justify-center items-center">
+                <div className="flex md:flex-row xs:flex-col xl:p-10 xs:p-4 xl:mx-20 justify-center items-center">
                         <div>
                             <img 
-                                className="rounded-xl lg:w-80 xs:w-64"
+                                className="rounded-xl md:w-80 xs:w-64"
                                 src={`https://image.tmdb.org/t/p/w1280/${data?.poster_path}`} 
                             />
                             {state.type === 'movie' ? (
-                                <span className="flex text-white space-x-5 font-semibold lg:text-lg xs:text-xs ml-3">
+                                <span className="flex text-white space-x-5 font-semibold lg:text-lg md:text-md xs:text-sm ml-3">
                                     <p>{data?.release_date?.split('-')[0]}</p>
                                     <p>{minutesToHours(data?.runtime)}</p>
                                 </span>
                                 ):(
-                                <span className="flex text-white space-x-5 font-semibold lg:text-lg xs:text-xs ml-3">
+                                <span className="flex text-white space-x-5 font-semibold lg:text-lg md:text-md xs:text-sm ml-3">
                                     <p>{data?.first_air_date?.split('-')[0]}</p>
                                     <p>{data?.seasons.length}{data?.seasons.length > 1 ? ' Seasons' : ' Season'}</p>
                                 </span>
                             )}
                         </div>
-                    <div className="flex lg:w-3/5 xs:w-full flex-col lg:mx-20 xs:mx-2 lg:items-start xs:items-center">
+                    <div className="flex lg:w-3/5 md:w-3/5 xs:w-full flex-col xl:mx-20 md:mx-10 xs:mx-2 md:items-start xs:items-center">
                         <span className="my-5">
                             {state.type === 'movie' ?
-                                <h2 className="text-2xl text-white font-bold text-3xl">{data?.title}</h2>
+                                <h2 className="2xl:text-3xl xl:text-2xl md:text-xl text-white font-bold">{data?.title}</h2>
                                 :
-                                <h2 className="text-2xl text-white font-bold text-3xl">{data?.name}</h2>
+                                <h2 className="2xl:text-3xl xl:text-2xl md:text-xl text-white font-bold">{data?.name}</h2>
                             }
-                            <p className="text-gray-100">{data?.tagline}</p>
+                            <p className="text-gray-100 xl:text-base lg:text-lg xs:text-sm">{data?.tagline}</p>
                         </span>
                         <div className="lg:flex my-5">
                             <div className="space-y-5">
                                 <span>
-                                    <h3 className="text-white font-bold lg:text-lg xs:text-sm">Overview</h3>
-                                    <p className="text-gray-100 lg:text-lg xs:text-xs font-semibold">{data?.overview}</p>
+                                    <h3 className="text-white font-bold lg:text-lg sm:text-sm xs:text-sm">Overview</h3>
+                                    <p className="text-gray-100 xl:text-lg lg:text-base sm:text-sm xs:text-xs font-semibold">{data?.overview}</p>
                                 </span>
                                 <ShowsRating data={data} />
                                 <StreamingServices 
@@ -141,18 +141,24 @@ const Details = ({ location }) => {
                                     providers={providers}
                                     options={options}
                                 />
-                                <Link className="flex text-white w-28 py-1 px-2 rounded-sm space-x-2 font-semibold" to={data?.homepage} target="_blank">
-                                    <HiOutlineLink />
-                                    <p className="text-white text-center">Website</p> 
-                                </Link>
                             </div>
-                            <div className="flex lg:flex-col text-white lg:ml-5">
-                                <p className="font-bold lg:text-sm xs-text-xs">Genres</p>
-                                {data?.genres.map((genre) => (
-                                    <p className="border border-green-300 rounded-lg lg:mb-2 text-center px-2 xs:mx-1 xs:text-xs">{genre.name}</p>
-                                ))}
-                            </div> 
+                            <div className="flex flex-col lg:ml-5">
+                                <p className="font-bold text-white lg:text-base md:text-sm xs:text-xs lg:mb-2">Genres</p>
+                                <div className="flex lg:flex-col">
+                                    {data?.genres.map((genre) => (
+                                        <div className="flex xs:mr-2 xl:mr-0">
+                                            <p className="lg:border w-full lg:border-green-300 rounded-lg lg:mb-2 xs:text-green-500 lg:text-white lg:text-center lg:px-2 xl:text-base lg:text-sm xs:text-xs">
+                                                {genre.name}
+                                            </p>
+                                        </div>
+                                    ))}
+                                </div> 
+                            </div>
                         </div>
+                        <Link className="flex xs:items-center text-white w-28 rounded-sm space-x-2 font-semibold" to={data?.homepage} target="_blank">
+                            <HiOutlineLink className="xs:text-sm sm:text-sm lg:text-base"/>
+                            <p className="text-white text-center xs:text-sm sm:text-sm lg:text-base">Website</p> 
+                        </Link>
                     </div>
                 </div>
             </div>
@@ -163,7 +169,7 @@ const Details = ({ location }) => {
                     ):( 
                     <h2 className="text-gray-500 text-2xl">Similar series</h2>
                 )}
-                <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 xs:px-2 lg:px-10 lg:w-4/5">
+                <div className="grid xl:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 xl:px-10 lg:px-5 xs:px-2 xl:w-4/5">
                     {similar && 
                         similar.map((element) => (
                             <ShowCard key={element.id} item={element} type={state.type}/>
