1972264304ef126d57f5aefa9b42b55cda477e2a
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-11-27 21:23:03 -0600

    code formatting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
-import * as React from "react"
-import { useState } from "react";
+import React, { useState } from "react"
 import {  graphql, useStaticQuery } from "gatsby";
 import { useQuery } from "@apollo/client";
 
--- src/pages/search/[name].jsx
+++ src/pages/search/[name].jsx
@@ -31,17 +31,17 @@ const Search = ({ params }) => {
 	if (status === "loading") return <Skeletons />;
 	return (
 		<div className="grid justify-items-center">
-		<div className="w-4/5">
-			<Navbar setMoviesView={setMoviesView} />
-		</div>
-		<h2 className="pt-2 flex sm:text-2xl xs:text-xl font-bold text-gray-600">
-			Search result for {`${param}`}
-		</h2>
-		<ShowsList
-			data={content}
-			type={moviesView ? "movie" : "tv"}
-			page="search"
-		/>
+			<div className="w-4/5">
+				<Navbar setMoviesView={setMoviesView} />
+			</div>
+			<h2 className="pt-2 flex sm:text-2xl xs:text-xl font-bold text-gray-600">
+				Search result for {`${param}`}
+			</h2>
+			<ShowsList
+				data={content}
+				type={moviesView ? "movie" : "tv"}
+				page="search"
+			/>
 		</div>
 	);
 };
