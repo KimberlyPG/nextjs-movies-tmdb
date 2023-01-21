9999d57c13885da209f3d74f4ff3794928bff8f5
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-01-21 15:36:07 -0600

    removing apollo client

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,15 +3,11 @@ import React from 'react';
 import Layout from './src/components/Layout';
 
 import './src/assets/style.css'
-import { ApolloProvider } from '@apollo/client';
-import { client } from './src/apollo/client';
 
 export const wrapRootElement = ({ element }) => {
   return (
     <Layout>
-      <ApolloProvider client={client}>
-        {element}    
-      </ApolloProvider>
+      {element}    
     </Layout>
   )
 };
