97fed3847efc6385dd2ed3bb64d5625ca2a3e0cb
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-02-21 19:57:12 -0600

    migrating file to typescript and typing the arguments

--- src/tmdb/detailsData.ts
+++ src/tmdb/detailsData.ts
@@ -1,4 +1,7 @@
-export const detailsData = async(contentId, type, setData, slug) => {
+import { Dispatch, SetStateAction } from "react";
+import { MoviesDetails, TvDetails } from "../types/tmdb-types";
+
+export const detailsData = async(contentId: string, type: string, setData: Dispatch<SetStateAction<MoviesDetails & TvDetails>> , slug: string) => {
     await fetch(`https://api.themoviedb.org/3/${type}/${contentId}${slug}?api_key=${process.env.apiKey}&language=en-US`) 
     .then((response) => {
         if (!response.ok) {
