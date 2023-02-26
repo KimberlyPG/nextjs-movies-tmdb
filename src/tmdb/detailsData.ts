71cfaaa9b93fb0499fe5cb6cf71055a1fb24320d
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-02-25 23:45:24 -0600

    types for the showsdata function arguments

--- src/tmdb/detailsData.ts
+++ src/tmdb/detailsData.ts
@@ -1,7 +1,12 @@
 import { Dispatch, SetStateAction } from "react";
 import { MoviesDetails, TvDetails } from "../types/tmdb-types";
 
-export const detailsData = async(contentId: string, type: string, setData: Dispatch<SetStateAction<MoviesDetails & TvDetails>> , slug: string) => {
+export const detailsData = async(
+  contentId: string, 
+  type: string, 
+  setData: Dispatch<SetStateAction<MoviesDetails & TvDetails>> , 
+  slug: string
+  ) => {
     await fetch(`https://api.themoviedb.org/3/${type}/${contentId}${slug}?api_key=${process.env.apiKey}&language=en-US`) 
     .then((response) => {
         if (!response.ok) {
